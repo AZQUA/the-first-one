f1d3ca3311ef9e1214dcc53f06bfcc955922bee2
Ange Leyre <leyre.ange@gmail.com>
2026-09-16 09:57:18 +0200

    feat: add calculator.ts and update readme.md

--- README.md
+++ README.md
@@ -5,4 +5,10 @@ The only-one collab-project
 
 git clone https://github.com/AZQUA/the-first-one.git
 
+## Usage
+
+a and b should be number
+
+add : add(a,b) -> a+b
+substract : substract(a,b -> a-b
 
---
+++ calculator.ts
@@ -0,0 +1,14 @@
+// calculator.ts
+export function add(a: any, b: any): number {
+	if (typeof a !== 'number' || typeof b !== 'number') {
+		throw new TypeError("Arguments must be numbers");
+	}
+	return a + b;
+}
+
+export function subtract(a: any, b: any): number {
+	if (typeof a !== 'number' || typeof b !== 'number') {
+		throw new TypeError("Arguments must be numbers");
+	}
+	return a - b;
+}
